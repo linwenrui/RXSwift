7d53b7c3572b412ed8942383966cd523f9250fe5
林文锐 <linwenrui@benbun.com>
2018-09-11 22:59:22 +0800

    pod

---
+++ RXSwiftBasics/Pods/Target Support Files/Pods-RXSwiftBasics/Pods-RXSwiftBasics-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RXSwiftBasics : NSObject
+@end
+@implementation PodsDummy_Pods_RXSwiftBasics
+@end
--- RXSwiftBasics/RXSwiftBasics/ViewController.swift
+++ RXSwiftBasics/RXSwiftBasics/ViewController.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 
+/// RxSwift: 它只是基于 Swift 语言的 Rx 标准实现接口库,所以 RxSwift 里不包含任何 Cocoa或者 UI 方面的类
+/// RxCocoa: 是基于 RxSwift针对于 iOS开发的一个库,它通过 Extention的方法给原生的比如 UI 控件添加了 Rx的特性,使得我们更容易订阅和响应这些控件的事件
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    
 }
 
